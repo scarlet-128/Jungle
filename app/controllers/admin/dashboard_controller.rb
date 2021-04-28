f3d379d4986d96f3756887fd6b4f18b9ca95f539
Scarlet Shao <scarletshao@gmail.com>
2021-04-28 00:13:48 +0000

    add the admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   def show
+  #   logger.debug "env:#{ENV['ADMIN_USERNAME']}"
+  # puts ENV['ADMIN_PASSWORD']
+    @number_of_products = Product.all.count
+    @number_of_categories = Category.all.length
   end
 end
